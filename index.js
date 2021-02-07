93ff90d35b2c6fcc3ff1e502e5f5ed6b1661c889
Derek Yu <yuderekyu@gmail.com>
2021-02-06 23:54:29 -0600

    rework nav. add js script for animation.

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * Toggle a css class based on visibility in viewport.
+ * Inspiration: https://github.blog/2021-01-29-making-githubs-new-homepage-fast-and-performant/
+ * @param {} entries
+ * @param {} observer
+ */
+const animationObserver = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    entry.target.classList.toggle('animate', entry.isIntersecting);
+  });
+}, { threshold: 0.5 });
+
+const homeButton = document.querySelector('.home');
+animationObserver.observe(homeButton);
