91491d61f80f82d5e2f91eab48e02c1748ff7423
Derek Yu <yuderekyu@gmail.com>
2021-04-18 17:31:04 -0500

    add animation to name. update nav link's hover color. remove deprecated iframe attribute

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 /**
  * Toggle a css class based on visibility in viewport.
  * Inspiration: https://github.blog/2021-01-29-making-githubs-new-homepage-fast-and-performant/
- * @param {} entries
- * @param {} observer
  */
 const callback = (entries, observer) => {
   entries.forEach(entry => {
@@ -16,3 +14,5 @@ const callback = (entries, observer) => {
 const animationObserver = new IntersectionObserver(callback, { threshold: 0.5 });
 const bookCover = document.querySelector('.book-cover');
 animationObserver.observe(bookCover);
+const name = document.querySelector('.name');
+animationObserver.observe(name);
