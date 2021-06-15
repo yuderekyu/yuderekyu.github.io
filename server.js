048bcb8338419d74b621e8e111e092df169d37d9
Derek Yu <yuderekyu@gmail.com>
2021-06-15 14:21:00 -0500

    Set minimum node version to 14 (#30)
    * [styles] update padding and margins.
    
    * expect >= node 14. cleanup server

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
 
+const app = express();
 app.use(express.static(__dirname));
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+const port = process.env.PORT ?? 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
