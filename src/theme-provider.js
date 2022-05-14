18f650cf73d31a5594aec74e396c551da5b42bc8
Derek Yu <yuderekyu@gmail.com>
2022-05-14 13:16:41 -0500

    [placeholder] initialize toggle

--- src/theme-provider.js
+++ src/theme-provider.js
@@ -1,18 +1,25 @@
 /**
  * @TODO: Toggle themes, utilizing localize storage to store the current theme value.
  */
+const getTheme = () => {
+  const theme = window.localStorage.getItem(themeKey);
+  console.log("theme = ", theme);
+  return theme;
+};
+
 const handleThemeSwitch = () => {
+  // get the toggle
   const mainElement = document.querySelector('.main');
+  // get the current theme
   console.log("mainElement = ", mainElement);
+  // if theme is dark, remove it
+
+  // else, apply it
   mainElement.classList.add('dark');
   window.localStorage.setItem(themeKey, 'dark');
 };
 
 const themeKey = 'yuderekyu.github.io.theme';
-const checkTheme = () => {
-  const theme = window.localStorage.getItem(themeKey);
-  console.log("theme = ", theme);
-};
 
 export {
   checkTheme,
