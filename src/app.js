2270731510184ae65d019a557e11ae4687ae991c
Derek Yu <yuderekyu@gmail.com>
2022-05-14 15:37:11 -0500

    enable dark theme

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import animatePage from '/src/animate-page.js';
+import initializeTheme from '/src/initialize-theme.js';
 
 window.onload = () => {
+  initializeTheme();
   animatePage({ threshold: 0.45 });
 };
