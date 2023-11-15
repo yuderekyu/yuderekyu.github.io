a233b9e70f564e47d21dd3d9766d2e88dfb6c6c2
Derek Yu <yuderekyu@gmail.com>
2023-11-14 19:18:45 -0600

    swap window.onload to DOMContentLoaded

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 import animatePage from '/src/animate-page.js';
 import setupThemeHandler from '/src/theme-handler.js';
 
-window.onload = () => {
+const initialize = () => {
   setupThemeHandler();
   animatePage({ threshold: 0.45 });
+};
+
+if (document.readyState === 'loading') {
+  document.addEventListener('DomContentLoaded', initialize);
+} else {
+  initialize();
 };
